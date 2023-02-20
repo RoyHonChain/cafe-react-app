f3de878783dd00d28b9d6c50c2b69168372691c6
RoyHonChain <roy.huang.block@gmail.com>
2023-02-21 03:14:59 +0800

    add some hint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ function Home() {
   return (
     <div className='Home'>
         <h1>Sorry I'm homeless... lol</h1>
+        <h3>All the Smart Contract is on Goerli.</h3>
+        <h5>Please make sure you change to the right network.</h5>
         <img src={soon} />
     </div>
   );
